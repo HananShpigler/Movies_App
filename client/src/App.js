f7bd417e502fbcd702509d027bb2a0dfb780f281
HananShpigler <shpiglerhanan123@gmail.com>
2021-05-15 19:29:44 +0300

    installing react/dependencies, cleaning up files/folders ...

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="app">testing</div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Header.css";
+
+const Header = () => {
+  return <span className="header">🎬 Movies App 🎬</span>;
+};
+
+export default Header;

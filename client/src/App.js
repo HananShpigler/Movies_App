1721eaeae89e57771659f7f97605b2ffd3636e6e
HananShpigler <shpiglerhanan123@gmail.com>
2021-05-15 20:16:49 +0300

    added components/pages/header/bottom navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
 import Header from "./components/Header/Header";
+import BottomNavigation from "./components/BottomNavigation/BottomNavigation";
+import Trending from "./pages/Trending/Trending";
+import Movies from "./pages/Movies/Movies";
+import Series from "./pages/Series/Series";
+import Search from "./pages/Search/Search";
+
 import "./App.css";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Container } from "@material-ui/core";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <div className="app">testing</div>
-    </>
+
+      <div className="app">
+        <Container>
+          <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
+
+      <BottomNavigation />
+    </BrowserRouter>
   );
 }
 

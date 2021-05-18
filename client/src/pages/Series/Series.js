a60d31e24ee32b66bd85839dc1b5422c223ac68a
HananShpigler <shpiglerhanan123@gmail.com>
2021-05-18 10:53:47 +0300

    tv series working

--- client/src/pages/Series/Series.js
+++ client/src/pages/Series/Series.js
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import useGenres from "../../CustomHook/useGenre";
+import Genres from "../../components/Genres/Genres";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import CustomPagination from "../../components/CustomPagination/CustomPagination";
 
 const Series = () => {
+  const [genres, setGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [totalPages, setTotalPages] = useState();
+  const urlGenre = useGenres(selectedGenres);
+
+  const getSeries = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${currentPage}&with_genres=${urlGenre}`
+    );
+    setContent(data.results);
+    setTotalPages(data.total_pages);
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    getSeries();
+    // eslint-disable-next-line
+  }, [currentPage, urlGenre]);
+
   return (
     <div>
-      <span className="pageTitle">Tv Series</span>
+      <span className="pageTitle">TV Series</span>
+      <Genres
+        type="tv"
+        selectedGenres={selectedGenres}
+        setSelectedGenres={setSelectedGenres}
+        genres={genres}
+        setGenres={setGenres}
+        setCurrentPage={setCurrentPage}
+      />
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type="tv"
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+      {totalPages > 1 && (
+        <CustomPagination
+          setCurrentPage={setCurrentPage}
+          totalPages={totalPages}
+        />
+      )}
     </div>
   );
 };
